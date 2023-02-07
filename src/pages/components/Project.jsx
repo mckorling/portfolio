799f8f914fabf13bed85406122f972caea1338bd
Megan Korling <korlingm@gmail.com>
2023-02-06 20:31:44 -0800

    Style projects

--- src/pages/components/Project.jsx
+++ src/pages/components/Project.jsx
@@ -1,13 +1,19 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../styles/Project.css"
 
 const Project = (props) => {
     return (
-        <div>
-            <h2>{props.title}</h2>
-            <p>{props.tech}</p>
-            <p>{props.description}</p>
-            <a href={props.github} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+        <div className="singleProject">
+            <div className="textContainer">
+                <h2>{props.title}</h2>
+                <p>{props.tech}</p>
+                <p>{props.description}</p>
+                <a href={props.github} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+            </div>
+            <div>
+                <img src={props.image} alt="describing project"/>
+            </div>
         </div>
     )
 }
