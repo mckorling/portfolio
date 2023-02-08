61195c270c2f09f5e648b106d1b45e0e1fc9867d
Megan Korling <korlingm@gmail.com>
2023-02-07 17:18:32 -0800

    Change to more readable font and add text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,17 +7,22 @@ const About = () => {
             <h2>Hello, World!</h2>
             <div className='info'>
                 <img src={headshot} alt="headshot of me" />
-                <p className='aboutText'>
-                    I am a full stack developer who is passionate about learning and working on a 
-                    team that encourages collaboration, questions, and growth. I have worked in 
-                    both Travel and Education in the past and would love to work on something that 
-                    helps traveling and/or education be simple and accessible for all. At the end 
-                    of the day though, I thrive when I can build products that are helpful and 
-                    meaningful to others. I like to be behind the scenes thinking about the bigger 
-                    picture- how can a user get the most efficient and smooth experience from this 
-                    product? I enjoy working in Python, Java, JavaScript, and React, and am always 
-                    excited to learn new languages and frameworks as I grow.
-                </p>
+                <div className='aboutText'>
+                    <p>
+                        I am a full stack developer who is passionate about learning and working on a 
+                        team that encourages collaboration, questions, and growth. I have worked in 
+                        both Travel and Education in the past and would love to work on something that 
+                        helps traveling and/or education be simple and accessible for all. 
+                    </p>
+                    <p>
+                        At the end of the day though, I thrive when I can build products that are helpful and 
+                        meaningful to others. I like to be behind the scenes thinking about the bigger 
+                        picture- how can a user get the most efficient and smooth experience from this 
+                        product? I enjoy working in Python, Java, JavaScript, and React, and am always 
+                        excited to learn new languages and frameworks as I grow.
+                    </p>
+                    
+                </div>
             </div>
         </div>
     )
