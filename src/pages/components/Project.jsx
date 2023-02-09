dea06d7fb0e6093a40922596a819f894fc3aca85
Megan Korling <korlingm@gmail.com>
2023-02-08 20:30:33 -0800

    Hamburger display and css set up, no js functionality yet

--- src/pages/components/Project.jsx
+++ src/pages/components/Project.jsx
@@ -8,14 +8,12 @@ const Project = (props) => {
         <section className="project__item">
             <h3>{props.title}</h3>
             <p className="project__item--tech">
-                
                 {props.tech}
             </p>
             <p className="project__item--description">
                 <a className="social" href={props.github} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
                 {props.description}
             </p>
-            
             <img src={props.image} alt="describing project" className="project__item--img"/>
         </section>
     )
