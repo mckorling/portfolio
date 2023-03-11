5cf1be3abe9a5ff65c3d7f9d6fc659c735fd2337
Megan Korling <korlingm@gmail.com>
2023-03-10 16:29:08 -0800

    Add active and demo link to relevant projects

--- src/pages/components/ProjectItem.jsx
+++ src/pages/components/ProjectItem.jsx
@@ -4,6 +4,8 @@ import "../../styles/ProjectItem.css"
 import "../../styles/App.css"
 
 const Project = (props) => {
+    const active = props.activeLink ? <a className="active" href={props.activeLink} rel="noreferrer" target="_blank">View</a> : <p></p>
+
     return (
         <section className="project__item">
             <h3>{props.title}</h3>
@@ -13,6 +15,7 @@ const Project = (props) => {
             <p className="project__item--description">
                 <a className="social" href={props.github} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
                 {props.description}
+                {active}
             </p>
             <img src={props.image} alt="describing project" className="project__item--img"/>
         </section>
